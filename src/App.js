54f4478c5653be0a149ad43eeda367c24b2ab096
Onur Simsek <onur.simsekk3@gmail.com>
2021-12-19 19:03:09 +0300

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+// import { Button } from "semantic-ui-react";
 
 function App() {
-  return <div className="App">TEST</div>;
+  return (
+    <div className="App">
+      <h1>Textify</h1>
+    </div>
+  );
 }
 
 export default App;
